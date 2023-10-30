fb60b2277c8c8d24b435911ba124d0051b96e19e
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2023-10-30 13:17:25 -0300

    feat: create notify method in event dispatcher

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import { SendEmailWhenProductIsCreatedHandler } from '../product/handler/send-email-when-product-Is-created-handler'
+import { ProductCreatedEvent } from '../product/product-created-event'
 import { EventDispatcher } from './event-dispatcher'
 
 describe('Domain events tests', () => {
@@ -30,5 +31,23 @@ describe('Domain events tests', () => {
 
     eventDispatcher.unregisterAll()
     expect(eventDispatcher.getEventHandlers.ProductCreatedEvent).toBeUndefined()
+  })
+
+  it('should notify all event handlers', () => {
+    const eventDispatcher = new EventDispatcher()
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+    const spyEventHandler = jest.spyOn(eventHandler, 'handler')
+    eventDispatcher.register('ProductCreatedEvent', eventHandler)
+    expect(eventDispatcher.getEventHandlers.ProductCreatedEvent[0]).toMatchObject(eventHandler)
+
+    const productCratedEvent = new ProductCreatedEvent({
+      name: 'product 1',
+      description: 'product 1 description',
+      price: 10.0
+    })
+
+    // quando o notify for chamado o SendEmailWhenProductIsCreatedHandler.handler() deve ser chamado
+    eventDispatcher.notify(productCratedEvent)
+    expect(spyEventHandler).toHaveBeenCalled()
   })
 })
